b50e6ac647475086e6ad576e43046a3b28591231
ecigwegbu <igwegbu@gmail.com>
2022-11-29 19:01:48 +0100

    15.remove at position - case of negative n

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def remove_char_at(st, n):
-    return f"{st[:n]}{st[n + 1:]}"
+    if n < 0 or n >= len(st):
+        return st
+    else:
+        return f"{st[:n]}{st[n + 1:]}"
