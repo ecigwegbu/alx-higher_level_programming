00eed81348a572a8d3d608d84173a837d7f740c8
ecigwegbu <igwegbu@gmail.com>
2022-12-04 22:22:46 +0100

    13. Linked list palindrome - v2

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 
+int check_item(listint_t **head, int idx, int idx_mirror);
 int get_item(listint_t **head, int idx);
 int get_len(listint_t **head);
 
@@ -26,11 +27,44 @@ int is_palindrome(listint_t **head)
 	for (i = 0; i <= stop_at; i++)
 	{
 		i_mirror = len - (i + 1);
-		if (get_item(head, i) != get_item(head, i_mirror))
+		/*if (get_item(head, i) != get_item(head, i_mirror)) */
+
+		/* compare i and i_mirror in one step --debug */
+		if (!check_item(head, i, i_mirror))
 		return (0); /* not a palindrome */
 	}
 
 	return (1);
+}
+
+/**
+ * check_item - check item in a singly linked list against mirror position
+ * @head: address of the pointer to the head of the linked list
+ * @idx: index position, 0-based
+ * @idx_mirror: mirror index position, 0-based
+ * Return: 0 if not matched, 1 if matched
+ */
+int check_item(listint_t **head, int idx, int idx_mirror)
+{
+	listint_t *ptr = *head;
+	int pos, n1, n2;
+
+	for (pos = 0; pos < idx_mirror; pos++, ptr = ptr->next)
+	{
+		if (ptr == NULL)
+		{
+			perror("get_item");
+			exit(EXIT_FAILURE);
+		}
+
+		if (pos == idx)
+		{
+			n1 = ptr->n;
+		}
+	}
+	n2 = ptr->n;
+
+	return (n1 == n2 ? 1 : 0);
 }
 
 /**
