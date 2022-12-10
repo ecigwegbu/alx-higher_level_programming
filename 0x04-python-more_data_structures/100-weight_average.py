351e00239e2c44ea182063e65b91b26e543ae740
ecigwegbu <igwegbu@gmail.com>
2022-12-11 00:13:34 +0100

    13. Weighted average!

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
     weights = [w[1] for w in my_list]
     scores_times_weights = [y[0] * y[1] for y in my_list]
     return sum(scores_times_weights) / sum(weights)
