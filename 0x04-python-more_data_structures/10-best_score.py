deeda4be2f18c2eed050e1c2962bc33889d449c4
ecigwegbu <igwegbu@gmail.com>
2022-12-10 11:26:20 +0100

    10. Best score - empty case

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,6 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
+    if len(a_dictionary) == 0:
+        return None
     return sorted(a_dictionary.items(), key=lambda item: item[1])[-1][0]
