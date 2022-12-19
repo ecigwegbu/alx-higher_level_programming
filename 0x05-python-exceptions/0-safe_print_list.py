8a15deea356680313a047b16987a5be13d19baaf
ecigwegbu <igwegbu@gmail.com>
2022-12-19 20:14:06 +0100

    Safe list printing - string type

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         return 0
     for i in range(x):
         try:
-            print(my_list[i], sep="", end="")
+            print(str(my_list[i]), sep="", end="")
         except IndexError:
             break
         else:
