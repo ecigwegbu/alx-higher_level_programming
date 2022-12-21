bae52243c78102e1a7c70e3644c9293f5b2b1e91
ecigwegbu <igwegbu@gmail.com>
2022-12-21 01:01:48 +0100

    0. My first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+""" This module ocntains one class
+    Class:
+        Square
+"""
+
+
 class Square:
+
     """This is an empty Square class
     It defines a Square."""
+
     pass
