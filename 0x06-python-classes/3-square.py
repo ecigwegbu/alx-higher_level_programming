8d835a08d9f239ca4578a5925bdbc35f9e208e61
ecigwegbu <igwegbu@gmail.com>
2022-12-21 01:51:14 +0100

    3. Area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" This module contains one class
+    Class:
+        Square
+"""
+
+
+class Square:
+    """This is square contains one private attribute
+    It defines a Square."""
+
+    def __init__(self, size=0):
+        """This function initialises the object"""
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """ Determine the area of the square
+        Return: area of square. """
+
+        return self.__size * self.__size
