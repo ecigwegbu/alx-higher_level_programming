cf6341013bc96b529f969ea30e31396805e5e23b
ecigwegbu <igwegbu@gmail.com>
2022-12-21 01:22:32 +0100

    1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" This module contains one class
+    Class:
+        Square
+"""
+
+
+class Square:
+    """This is square contains one private attribute
+    It defines a Square."""
+
+    def __init__(self, size):
+        """This function initialises the object"""
+
+        self.__size = size
