af38ac5447b3fe98dbf415fefa09425763800796
ecigwegbu <igwegbu@gmail.com>
2023-01-03 05:58:48 +0100

    10. N queens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,80 +1,14 @@
 #!/usr/bin/python3
-""" Module defines Rectangle Class """
+""" THis module solves the N Queens puzzle """
 
 
-class Rectangle:
-    """ Rectangle Class Based on 2-Rectangle"""
-
-    def __init__(self, width=0, height=0):
-        """ Constructor """
-
-        if type(width) != int:
-            raise TypeError("width must be an integer")
-        elif width < 0:
-            raise ValueError("width must be >= 0")
-        elif type(height) != int:
-            raise TypeError("height must be an integer")
-        elif height < 0:
-            raise ValueError("height must be >= 0")
-        self.__width = width
-        self.__height = height
-
-    @property
-    def width(self):
-        """ width propert getter """
-
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """ widgth property setter"""
-
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        """ height propert getter """
-
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """ height property setter"""
-
-        if type(value) != int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
-
-    def area(self):
-        """ Determine the area of the rectangle
-        Return: area of rectangle. """
-
-        return self.__width * self.__height
-
-    def perimeter(self):
-        """ Determine the perimeter of the rectangle
-        Return: perimeter of rectangle. """
-
-        if self.__width == 0 or self.__height == 0:
-            return 0
-        else:
-            return 2 * (self.__width + self.__height)
-
-    def __str__(self):
-        """ return string representation of Rectangle """
-
-        if self.__width == 0 or self.__height == 0:
-            return ""
-        __str = ""
-        for i in range(self.__height):
-            for j in range(self.__width):
-                __str = __str + "#"
-            if i != self.__height - 1:
-                __str = __str + "\n"
-        return __str
+import sys
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    exit(1)
+if sys.argv[1].isdigit != True:
+    print("N must be a number")
+    exit(1)
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
