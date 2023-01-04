c1846556b744c1923d53cc89195035759719c6e0
ecigwegbu <igwegbu@gmail.com>
2023-01-04 23:00:21 +0100

    1. Divide a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,28 +1,47 @@
 #!/usr/bin/python3
-""" This module is for a function that
-adds two numbers a and b
-Prototype: def add_integer(a, b=98):
-a and b must be integers or floats, otherwise raise
-a TypeError exception with the message a must be an
-integer or b must be an integer
-a and b must be first casted to integers if they are float
-Returns an integer: the addition of a and b
+"""
+Write a function that divides all elements of a matrix.
+Prototype: def matrix_divided(matrix, div):
+matrix must be a list of lists of integers or floats, otherwise raise a
+TypeError exception with the message matrix must be a matrix (list of lists)
+of integers/floats
+Each row of the matrix must be of the same size, otherwise raise a TypeError
+exception with the message Each row of the matrix must have the same size
+div must be a number (integer or float), otherwise raise a TypeError exception
+with the message div must be a number
+div can’t be equal to 0, otherwise raise a ZeroDivisionError exception with
+the message division by zero
+All elements of the matrix should be divided by div, rounded to 2 decimal
+places
+Returns a new matrix
 You are not allowed to import any module
 """
 
 
-def add_integer(a, b=98):
-    """ This function adds two numbers
-    and returns their sum.
-    The numbers must be integers
+def matrix_divided(matrix, div):
+    """ This function divides a matrix by a number
+    The number must be a float or int.
+    Returns a new matrix.
     """
 
-    if type(a) != int and type(a) != float:
-        raise TypeError("a must be an integer")
-    if type(b) != int and type(b) != float:
-        raise TypeError("b must be an integer")
-    if type(a) == float:
-        a = int(a)
-    if type(b) == float:
-        b = int(b)
-    return (a + b)
+    msg = "matrix must be a matrix (list of lists) of integers/floats"
+    if type(matrix) != list:
+        raise TypeError(msg)
+    if len(matrix) == 0:
+        raise TypeError(msg)
+    for elt in matrix:
+        if type(elt) != list or len(elt) == 0:
+            raise TypeError(msg)
+    cols = len(matrix[0])
+    for elt in matrix:
+        if len(elt) != cols:
+            raise TypeError("Each row of the matrix must have the same size")
+        for num in elt:
+            if (type(num) != int and type(num) != float):
+                raise TypeError(msg)
+
+    if type(div) != int and type(div) != float:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    return [[round(num / div, 2) for num in rw] for rw in matrix]
