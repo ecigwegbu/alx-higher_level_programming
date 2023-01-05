47eb6f040b6ea92388dc6f743a3280427cfa858a
ecigwegbu <igwegbu@gmail.com>
2023-01-05 08:05:51 +0100

    3. Print square

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,28 +1,35 @@
 #!/usr/bin/python3
-""" This module is for a function that
-adds two numbers a and b
-Prototype: def add_integer(a, b=98):
-a and b must be integers or floats, otherwise raise
-a TypeError exception with the message a must be an
-integer or b must be an integer
-a and b must be first casted to integers if they are float
-Returns an integer: the addition of a and b
+"""
+Write a function that prints a square with the character #.
+
+Prototype: def print_square(size):
+size is the size length of the square
+size must be an integer, otherwise raise a TypeError exception
+with the message size must be an integer
+if size is less than 0, raise a ValueError exception with the
+message size must be >= 0
+if size is a float and is less than 0, raise a TypeError exception
+with the message size must be an integer
 You are not allowed to import any module
 """
 
 
-def add_integer(a, b=98):
-    """ This function adds two numbers
-    and returns their sum.
-    The numbers must be integers
-    """
+def print_square(size):
+    """This function prints a square
+    Tests are created to ensure it works.
+    It defines a Square."""
 
-    if type(a) != int and type(a) != float:
-        raise TypeError("a must be an integer")
-    if type(b) != int and type(b) != float:
-        raise TypeError("b must be an integer")
-    if type(a) == float:
-        a = int(a)
-    if type(b) == float:
-        b = int(b)
-    return (a + b)
+    if size == 0:
+        return
+
+    if type(size) != int or (type(size) == float and int(size) != size):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    if type(size) == float and size < 0:
+        raise TypeError("size must be an integer")
+
+    for line in range(size):
+        print("#" * size)
+    return
