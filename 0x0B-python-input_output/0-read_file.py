32f5b1ccb85b2706bbd867a634935113d9b2976a
ecigwegbu <igwegbu@gmail.com>
2023-01-10 10:16:01 +0100

    0. Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" This module is for a function
+That reads data from a file and
+prints to standout
+"""
+
+
+def read_file(filename=""):
+    """ This function reads input from a file and
+    prints it to stdout"""
+
+    if filename == "":
+        return
+
+    with open(filename, 'r') as myfile:
+        for line in myfile:
+            print(line, end="")
+
+    return
