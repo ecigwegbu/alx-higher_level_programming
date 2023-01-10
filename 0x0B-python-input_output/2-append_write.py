f9fe6f579ab2c3a1374ab5e800af0edad50c8a37
ecigwegbu <igwegbu@gmail.com>
2023-01-10 11:31:10 +0100

    0.Read file v2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" This module is for a function
+that appends data to a file and
+"""
+
+def append_write(filename="", text=""):
+    """ This function appends data to a file.
+    """
+
+    if filename == "" or text == "":
+        return
+
+    with open(filename, 'a') as f:
+            return f.append(text)
