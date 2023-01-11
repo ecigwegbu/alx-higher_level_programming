1c4da7bf29b78cefe269f7880051d44b9c30f93e
ecigwegbu <igwegbu@gmail.com>
2023-01-11 02:19:19 +0100

    11. Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" This module contains a class that defines a student
+Student class is to be serialised to json later.
+"""
+
+
+class Student:
+    "This class defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """constructor for the object"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        return
+
+    def to_json(self, attrs=None):
+        """ serialise self to json """
+
+        isStr = True
+        if type(attrs) == list:
+            isStr = True
+            for elt in attrs:
+                if type(elt) != str:
+                    isStr is False
+        if isStr is False or attrs is None:
+            return self.__dict__
+        else:
+            return {at: self.__dict__[at] for at in attrs if
+                    at in self.__dict__}
+
+    def reload_from_json(self, json):
+        """ load json attributes into self """
+
+        for att in json:
+            self.__setattr__(att, json[att])
+        return
