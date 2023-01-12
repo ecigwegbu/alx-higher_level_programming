713e65f28565444da4937a96412b5c90d9fa84c5
ecigwegbu <igwegbu@gmail.com>
2023-01-12 05:22:56 +0100

    Placeholders added

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" This module contains a function that
+Checks if an object is an instance of a class"""
+
+
+def is_same_class(obj, a_class):
+    """ Check if object is an instance of a class """
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
