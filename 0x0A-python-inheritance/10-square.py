d65422f64463d3a8083a445aea0658192e7af011
ecigwegbu <igwegbu@gmail.com>
2023-01-12 16:07:51 +0100

    10. Square #1

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,12 +1,61 @@
 #!/usr/bin/python3
-""" This module contains a function that
-Checks if an object is an instance of a class"""
+""" This module improves Basegeometry and Rectangle classes
+Square class inherits from Rectangle Class
+"""
 
 
-def is_same_class(obj, a_class):
-    """ Check if object is an instance of a class """
+class BaseGeometry:
+    """ BaseGeometry class """
 
-    if isinstance(obj, a_class):
-        return True
-    else:
-        return False
+    def area(self):
+        """ not yet implemented"""
+
+        raise Exception("area() is not implemented")
+        return
+
+    def integer_validator(self, name, value):
+        """ validate function """
+
+        if type(value) != int:
+            if type(value) == float:
+                if value % 1 != 0:
+                    raise TypeError(name + " must be an integer")
+            else:
+                raise TypeError(name + " must be an integer")
+        if value <= 0:
+            raise ValueError(name + " must be greater than 0")
+        return
+
+
+class Rectangle(BaseGeometry):
+    """ Inherits from BaseGeometry class"""
+
+    def __init__(self, width, height):
+        """ Construct rectangle """
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self._Rectangle__width = width
+        self._Rectangle__height = height
+
+    def area(self):
+        return self._Rectangle__width * self._Rectangle__height
+
+    def __str__(self):
+        return "[Rectangle] " + str(self._Rectangle__width) + "/" +\
+                str(self._Rectangle__height)
+
+
+class Square(Rectangle):
+    """ Inherits from Rectangle which in turn
+    inherits from BaseGeometry"""
+
+    def __init__(self, size):
+        """ Construct square """
+
+        super().__init__(size, size)
+        self._Square__size = size
+
+    def area(self):
+
+        return super().area()
