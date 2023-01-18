3e0627a9fa59680d5fd464fad9797a77ce5e1bd6
ecigwegbu <igwegbu@gmail.com>
2023-01-18 01:57:19 +0100

    5. Display #0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -96,3 +96,21 @@ class Rectangle(Base):
         """ Compute the area of the rectangle """
 
         return self.width * self.height
+
+    def __str__(self):
+        """ return string representation of Rectangle """
+
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        __str = ""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                __str = __str + "#"
+            if i != self.__height - 1:
+                __str = __str + "\n"
+        return __str
+
+    def display(self):
+        """ Display the rectabgle """
+
+        print(self)
