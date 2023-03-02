ed5c7bf079856d783d935ff190148ed82f8b8852
ecigwegbu <igwegbu@gmail.com>
2023-03-02 01:51:34 +0100

    7. Cities table

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,16 @@
+-- Create Cities table
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+	id INT
+		NOT NULL
+		AUTO_INCREMENT
+		UNIQUE KEY
+		PRIMARY KEY,
+	state_id INT
+		NOT NULL
+		FOREIGN KEY REFERENCES states(id)
+	name VARCHAR(256)
+		NOT NULL
+);
