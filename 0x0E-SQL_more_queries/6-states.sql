417477e5cf354ec7d3e02708df748db6db2eeef0
ecigwegbu <igwegbu@gmail.com>
2023-03-02 01:28:11 +0100

    6.States table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,12 @@
+-- Create db+table with unique id column that is prim key and can't be null
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states (
+	id INT
+		NOT NULL
+		AUTO_INCREMENT
+		UNIQUE KEY
+		PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
+);
