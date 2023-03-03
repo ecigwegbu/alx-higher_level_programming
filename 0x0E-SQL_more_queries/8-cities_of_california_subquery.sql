e8cf128ea6c0ddaf3ffec46c21dbe10f506f20f4
ecigwegbu <igwegbu@gmail.com>
2023-03-03 09:04:09 +0100

    8.Cities of California

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,14 +1,5 @@
 --  subquery cities of california
 
 SELECT id, name FROM cities
-	WHERE id='states.name'California
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-	id INT
-		NOT NULL
-		AUTO_INCREMENT
-		UNIQUE KEY
-		PRIMARY KEY,
-	name VARCHAR(256) NOT NULL
-);
+	WHERE state_id=
+		SELECT id FROM states WHERE name=California
