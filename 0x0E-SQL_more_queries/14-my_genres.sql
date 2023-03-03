dfb8d6450370a2538472f76b88adbc47f99bcd1d
ecigwegbu <igwegbu@gmail.com>
2023-03-03 17:41:07 +0100

    My Genres

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,8 @@
 -- My genres
 
 SELECT tv_genres.name FROM tv_genres
-	LEFT JOIN tv_show_genres
+	JOIN tv_show_genres
 	ON tv_show_genres.genre_id=tv_genres.id
-	WHERE tv_show_genres.show_id=
-	(SELECT id FROM tv_shows WHERE tv_shows.title='Dexter')
-	ORDER BY tv_genres.name ASC;
+	JOIN tv_shows
+	ON tv_shows.id=tv_show_genres.show_id
+	WHERE tv_shows.title='Dexter' ORDER BY tv_genres.name ASC;
