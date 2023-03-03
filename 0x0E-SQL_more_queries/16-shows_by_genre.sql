92ebc4159479641e56109d423cf415aee7571fc8
ecigwegbu <igwegbu@gmail.com>
2023-03-03 18:06:45 +0100

    16. List shows and genres

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -5,5 +5,4 @@ SELECT tv_shows.title, tv_genres.name FROM tv_shows
 	ON tv_show_genres.show_id=tv_shows.id
 	JOIN tv_genres
 	ON tv_genres.id=tv_show_genres.genre_id
-	WHERE tv_genres.name='Comedy'
-	ORDER BY tv_shows.title ASC, tv_genre.name ASC;
+	ORDER BY tv_shows.title ASC, tv_genres.name ASC;
