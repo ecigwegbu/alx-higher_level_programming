1d573e69ae9fca841f854649fb0b742b40135e0f
ecigwegbu <igwegbu@gmail.com>
2023-03-13 20:21:02 +0100

    2. Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-import { argv } from 'node:process';
+// import { argv } from 'node:process';
 
 let myVar;
-if (argv[2] == null) {
+if (process.argv[2] == null) {
   myVar = 'No argument';
-} else if (argv[3] == null) {
+} else if (process.argv[3] == null) {
   myVar = 'Argument found';
 } else {
   myVar = 'Arguments found';
