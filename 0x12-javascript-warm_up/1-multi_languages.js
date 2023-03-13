d718eb756c3288b3ca91a1970ff782a30ed33b4c
ecigwegbu <igwegbu@gmail.com>
2023-03-13 12:19:06 +0100

    1. 3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
-/*const myVar = 'JavaScript is amazing'; */
-console.log('C is fun\nPython is cool\nJavaScript is amazing');
+const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
+console.log(myVar);
