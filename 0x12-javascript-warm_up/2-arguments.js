1a9d9e12e1dcd2644871c2017da25938e961c03a
ecigwegbu <igwegbu@gmail.com>
2023-03-13 12:58:02 +0100

    2. Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,9 +4,9 @@ import { argv } from 'node:process';
 
 // console.log(argv.length);
 if (argv.length === 2) {
-	console.log('No argument');
+  console.log('No argument');
 } else if (argv.length === 3) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
