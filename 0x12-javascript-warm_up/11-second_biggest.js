d7ac1acd4f1d7695000e7109df3a43ba9856a179
ecigwegbu <igwegbu@gmail.com>
2023-03-14 09:07:19 +0100

    11.Second Largest Number

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,16 +1,19 @@
 #!/usr/bin/node
 // Compute the second biggest argument
 
-// get biggest
+// Edge cases first
 let max = parseInt(process.argv[2]);
-for (let i = 2; i < parseInt(process.argv.length); i++) {
-  if (parseInt(process.argv[i]) > max) max = parseInt(process.argv[i]);
+let secondMax = Number.MIN_SAFE_INTEGER;
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  for (let i = 2; i < parseInt(process.argv.length); i++) {
+    if (parseInt(process.argv[i]) > max) max = parseInt(process.argv[i]);
+  }
+  // console.log('Max: ', max);
+  for (let i = 2; i < parseInt(process.argv.length); i++) {
+    if (parseInt(process.argv[i]) > secondMax &&
+        parseInt(process.argv[i]) !== max) secondMax = parseInt(process.argv[i]);
+  }
+  console.log(secondMax);
 }
-
-// get second biggest
-let secondMax = parseInt(process.argv[2]);
-for (let i = 2; i < parseInt(process.argv.length); i++) {
-  if (parseInt(process.argv[i]) > secondMax &&
-      parseInt(process.argv[i]) !== max) secondMax = parseInt(process.argv[i]);
-}
-console.log(secondMax);
