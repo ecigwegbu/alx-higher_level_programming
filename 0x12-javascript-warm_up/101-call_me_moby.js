398cee0b458073eac9018a0b5ed21c4096547441
ecigwegbu <igwegbu@gmail.com>
2023-03-14 09:50:28 +0100

    16. Add me maybe

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// function add that is visible fom the outside
+// function that runs x times from outside
 
 exports.callMeMoby = function (x, theFunction) {
   for (let i = 0; i < x; i++) theFunction();
