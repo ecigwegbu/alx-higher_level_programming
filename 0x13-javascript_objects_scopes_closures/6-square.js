b41572d39664711dab931e011b927f191bf5aab9
ecigwegbu <igwegbu@gmail.com>
2023-03-15 12:36:40 +0100

    6.Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,5 +1,61 @@
 #!/usr/bin/node
 // An empty rectangle class
 
-class Rectangle {}
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    if (this.width && this.height) {
+      let rect = '';
+      for (let i = 0; i < this.width; i++) { // build one line
+        rect += 'X';
+      }
+      for (let j = 0; j < this.height; j++) { // output several lines
+        console.log(rect);
+      }
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    /* this.size = size; */
+  }
+
+  charPrint (c) {
+    /* !c ? c = 'X' : c; */
+    if (!c) {
+      this.print();
+    } else {
+      if (this.width && this.height) {
+        let rect = '';
+        for (let i = 0; i < this.width; i++) { // build one line
+          rect += 'C';
+        }
+        for (let j = 0; j < this.height; j++) { // output several lines
+          console.log(rect);
+        }
+      }
+    }
+  }
+}
+
 module.exports = Rectangle;
+module.exports = Square;
