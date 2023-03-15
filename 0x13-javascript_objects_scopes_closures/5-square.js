e11a892d9269bb2d96493cb0164337de79e706a6
ecigwegbu <igwegbu@gmail.com>
2023-03-15 05:59:54 +0100

    5 - Square

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,5 +1,46 @@
 #!/usr/bin/node
 // An empty rectangle class
 
-class Rectangle {}
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    if (this.width && this.height) {
+      let rect = '';
+      for (let i = 0; i < this.width; i++) { // build one line
+        rect += 'X';
+      }
+      for (let j = 0; j < this.height; j++) { // output several lines
+        console.log(rect);
+      }
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+  if (size > 0) {
+      super(size, size);
+      this.size = size;
+    }
+  }
+}
+
 module.exports = Rectangle;
+module.exports = Square;
