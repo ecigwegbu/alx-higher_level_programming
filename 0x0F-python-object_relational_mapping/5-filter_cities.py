ef21984db30bc3e59fc56b61d35594dedd9e86a7
ecigwegbu <igwegbu@gmail.com>
2023-03-19 06:18:52 +0100

    5. All cities by state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''Load and filter a database'''
+
+import MySQLdb
+from sys import argv
+import re
+
+if __name__ == "__main__":
+    if (len(argv) != 5):
+        exit()
+
+    mydb = MySQLdb.connect(host='localhost', user=argv[1],
+                           passwd=argv[2], db=argv[3], port=3306)
+    cur = mydb.cursor()
+
+    if (re.search('^[\\w ]*$', argv[4])):  # filter out possible SQL injection
+        cur.execute("SELECT cities.id, cities.name, states.name "
+                    "FROM cities JOIN states ON cities.state_id=states.id "
+                    "WHERE states.name = '%s' "
+                    "ORDER BY cities.id ASC" % argv[4])
+
+        # Print results
+        rows = cur.fetchall()
+        print(rows[0][1], end="")
+        # for i in range(1, 3):
+        for i in range(1, len(rows)):
+            print(', ', rows[i][1], end="")
+        print("")
+
+        # clean up
+        cur.close()
+        mydb.close()
