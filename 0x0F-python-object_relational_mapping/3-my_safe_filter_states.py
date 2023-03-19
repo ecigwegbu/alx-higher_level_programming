76f006092479b4e04cd65e39cab6c2bc5d03e6fd
ecigwegbu <igwegbu@gmail.com>
2023-03-19 03:17:37 +0100

    3. SQL Injection...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,24 +5,10 @@ import MySQLdb
 from sys import argv
 import re
 
-
-def addSlashes(string):
-    ''' add escape slashes to special characters in a string'''
-    if (re.search('^[\\w ]*$', string)):  # regex matches words and spaces only
-        return string
-    else:
-        return ""
-
-
 if __name__ == "__main__":
-    # print("Args :", argv[0], argv[1], argv[2])
     if (len(argv) != 5):
         exit()
 
-    # state = addSlashes(argv[4])
-    # print("Modified: ", state)
-    # print("QUERY: SELECT * FROM states WHERE name = '%s' ORDER BY id ASC"
-    #       % state)
     mydb = MySQLdb.connect(host='localhost', user=argv[1],
                            passwd=argv[2], db=argv[3], port=3306)
     cur = mydb.cursor()
