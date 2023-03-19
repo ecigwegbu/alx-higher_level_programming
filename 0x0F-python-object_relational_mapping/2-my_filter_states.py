30af60927b08eba73a88be0f7adc8c86991a2cf6
ecigwegbu <igwegbu@gmail.com>
2023-03-19 05:30:20 +0100

    2. Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,23 +3,24 @@
 
 import MySQLdb
 from sys import argv
+import re
 
 if __name__ == "__main__":
-    # print("Args :", argv[0], argv[1], argv[2])
     if (len(argv) != 5):
         exit()
 
     mydb = MySQLdb.connect(host='localhost', user=argv[1],
                            passwd=argv[2], db=argv[3], port=3306)
     cur = mydb.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
-                .format(argv[4]))
+    if (re.search('^[\\w ]*$', argv[4])):  # regex matches words and spcs only
+        cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+                    .format(argv[4]))
 
-    # Print results
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+        # Print results
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
 
-    # Clean up
-    cur.close()
-    mydb.close()
+        # clean up
+        cur.close()
+        mydb.close()
