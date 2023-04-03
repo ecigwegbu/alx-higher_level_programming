b08d3f852373383201b5ba42ab5baeb6870c7350
ecigwegbu <igwegbu@gmail.com>
2023-04-03 18:41:20 +0100

    3. cURL only methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
- # This bash script takes in a URL, sends a request to that URL,
+# This bash script takes in a URL, sends a request to that URL,
 curl -X "OPTIONS" "$1" -i 2> /dev/null | grep '^Allow' | cut -f1 -d: --complement
