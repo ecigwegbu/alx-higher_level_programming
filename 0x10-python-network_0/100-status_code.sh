79388c204cf6918bb7c4e4ce640811ae83ab3e6d
ecigwegbu <igwegbu@gmail.com>
2023-04-04 09:02:13 -0700

    7. Only status code

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This bash script takes in a URL, sends a request to that URL
-curl -w "%{response_code}" -o /dev/null 2> /dev/null "$1"
+curl -w "%{response_code}" -o /dev/null --stderr /dev/null "$1"
