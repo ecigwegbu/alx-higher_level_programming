013fa87700f1206611670dab56d8f6681c57686a
ecigwegbu <igwegbu@gmail.com>
2023-04-04 08:25:33 -0700

    6.Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,4 +4,12 @@
 
 def find_peak(list_of_integers):
     """ find peak in a list of unsorted integers"""
-    pass
+
+    if len(list_of_integers) == 0:
+        return None
+    maxi = list_of_integers[0]
+    for i in range(0, len(list_of_integers)):
+        if list_of_integers[i] < maxi:
+            return maxi
+        maxi = list_of_integers[i]
+    return maxi
