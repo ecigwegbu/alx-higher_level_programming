60e4ae4c22ddd72bb9390fe01bdda5b671b145fe
ecigwegbu <igwegbu@gmail.com>
2023-04-04 06:01:56 -0700

    5. cURL POST parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This bash script takes in a URL, sends a request to that URL
-curl -d email='test@gmail.com' -d subject='I will always be here for PLD' "$1"  2> /dev/null
+curl -d 'email=test@gmail.com' -d 'subject=I will always be here for PLD' "$1"  2> /dev/null
