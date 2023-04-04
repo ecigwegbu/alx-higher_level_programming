6eb776581e8493b1fab2a938debda0c8573034ab
ecigwegbu <igwegbu@gmail.com>
2023-04-04 11:02:35 -0700

    8. cURL a JSON file

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This bash script takes in a URL, sends a request to that URL
-curl -w "You got me!" "0.0.0.0:5000/catch_me" -so /dev/null
+curl -X PUT arg="You got me!" "0.0.0.0:5000/catch_me" -so /dev/null
