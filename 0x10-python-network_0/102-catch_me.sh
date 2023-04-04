041c18e7405020b9fa27583bc74c387c56646aed
ecigwegbu <igwegbu@gmail.com>
2023-04-04 10:15:16 -0700

    9. Catch me if you can!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This bash script takes in a URL, sends a request to that URL
-curl -s -w "%{stdout}" -w "You got me!\n" -o stdout "0.0.0.0:5000/catch_me"
+curl -w "You got me!" "0.0.0.0:5000/catch_me" -so /dev/null
