e879144d09b098836c42e1e62fad8a79209e0f6d
ecigwegbu <igwegbu@gmail.com>
2023-04-07 17:28:08 +0100

    0. What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,6 +4,10 @@
 
 import urllib.request
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    html = response.read()
-    print(html)
+req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+with urllib.request.urlopen(req) as response:
+    mydict = response.info().items()
+
+print("Body response:")
+for elt in mydict:
+    print(f"\t- {elt[0]}: {elt[1]}")
