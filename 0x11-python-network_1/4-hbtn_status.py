31b31185dbab7fb3073bd7a6fc1cef5e98158460
ecigwegbu <igwegbu@gmail.com>
2023-04-08 00:26:16 +0100

    4. What's my status? #1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""send request using requests module"""
+
+
+import requests
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+    print("Body response:")
+    print(f"\t- type: {type(response.text)}")
+    print(f"\t- content: {response.text}")
