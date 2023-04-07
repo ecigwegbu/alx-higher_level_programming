487ec05cd5a9bcb40ab327be82ce84c0bb6e23a3
ecigwegbu <igwegbu@gmail.com>
2023-04-07 21:09:17 +0100

    2. POST an email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Post data to a URL using urllib"""
+
+
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    data = {"email": "igwegbu@gmail.com"}
+    data_url_encoded = urllib.parse.urlencode(data)
+    data_url_encoded = urllib.parse.urlencode(data)
+    data_byte_encoded = data_url_encoded.encode("utf-8")
+    # urlopen is a function that takes a request object as parameter
+    # or takes url and other parameters
+    with urllib.request.urlopen(url, data=data_byte_encoded) as response:
+        print(response.read().decode("utf-8"))
