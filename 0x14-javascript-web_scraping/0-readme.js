f10e31c0d6492783dd3e3acb2a285b4d61b0807e
ecigwegbu <igwegbu@gmail.com>
2023-04-25 20:07:45 +0100

    0. Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
-//
+// read and print a file
+
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data.toString());
+  }
+});
