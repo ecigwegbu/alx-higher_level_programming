3fc6e2920cdb627dbdc4f88897c97b5ff41e3c75
ecigwegbu <igwegbu@gmail.com>
2023-05-05 20:50:16 +0100

    8. Star Wars movies

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,10 @@
+// download json data and extract all movie titles
+$(function () {
+  $.get('https://swapi-api.alx-tools.com/api/films/?format=json',
+    function (data) {
+      for (const movie of data.results) {
+        $('UL#list_movies').append('<li>' + `${movie.title}` + '</li>');
+      }
+    },
+    'json');
+});
